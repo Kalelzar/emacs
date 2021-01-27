a56a8b9bbac495773f5fff910e14e81be5c4f771
Kalelzar <natomanofglory@gmail.com>
2021-01-27 12:24:03 +0200

    Add a lot of stuff to custom.el. Really need to put them in config.org instead

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(calibredb-library-alist '(("~/Documents/Library/")))
  '(custom-safe-themes
    '("be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default))
  '(default-input-method "bulgarian-phonetic")
@@ -14,12 +15,14 @@
  '(ede-project-directories '("/home/kalelzar/Code/cpp/pulsebind"))
  '(global-undo-tree-mode t)
  '(global-whitespace-mode t)
+ '(org-agenda-files
+   '("~/Documents/tickler.org" "~/Documents/notes.org" "~/Documents/agenda.org"))
  '(org-format-latex-options
    '(:foreground default :background default :scale 2.0 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
                  ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(org-link-beautify-mode nil)
  '(package-selected-packages
-   '(cmake-mode cmake-project svg-tag-mode org-sidebar org-treeusage sbt-mode lsp-metals scala-mode scalariform cakecrumbs auto-rename-tag company-web web-beautify kaltask web-server json-mode json-snatcher json-reformat macrostep ghub treepy closql emacsql-sqlite emacsql git-commit with-editor transient swiper projectile helm-core popup async alert log4e gntp pkg-info epl shrink-path company-lsp company lsp-ui lsp-mode lv markdown-mode ht f s dash-functional dash framemove bar-cursor god-mode justify-kp slime magit counsel ivy helm novel-mode org-link-beautify all-the-icons memoize yasnippet quelpa-use-package treefactor rainbow-identifiers rainbow-blocks org-drill flymake-jslint org-wild-notifier org-alert all-the-icons-ivy yasnippet-snippets which-key wanderlust volatile-highlights use-package unicode-fonts undo-tree super-save stumpwm-mode srefactor smooth-scrolling smartparens slime-company scratch rainbow-mode rainbow-delimiters python-mode page-break-lines ox-epub org-superstar org-sticky-header org-plus-contrib nov hl-todo helm-swoop helm-projectile helm-ag forge flycheck fira-code-mode expand-region ewal-doom-themes elisp-slime-nav edit-server doom-modeline diminish crux counsel-projectile common-lisp-snippets cdlatex ccls c-eldoc avy auctex all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired activity-watch-mode))
+   '(centered-window rust-mode yaml-imenu flycheck-yamllint yaml-mode calibredb cmake-mode cmake-project svg-tag-mode org-sidebar org-treeusage sbt-mode lsp-metals scala-mode scalariform cakecrumbs auto-rename-tag company-web web-beautify kaltask web-server json-mode json-snatcher json-reformat macrostep ghub treepy closql emacsql-sqlite emacsql git-commit with-editor transient swiper projectile helm-core popup async alert log4e gntp pkg-info epl shrink-path company-lsp company lsp-ui lsp-mode lv markdown-mode ht f s dash-functional dash framemove bar-cursor god-mode justify-kp slime magit counsel ivy helm novel-mode org-link-beautify all-the-icons memoize yasnippet quelpa-use-package treefactor rainbow-identifiers rainbow-blocks org-drill flymake-jslint org-wild-notifier org-alert all-the-icons-ivy yasnippet-snippets which-key wanderlust volatile-highlights use-package unicode-fonts undo-tree super-save stumpwm-mode srefactor smooth-scrolling smartparens slime-company scratch rainbow-mode rainbow-delimiters python-mode page-break-lines ox-epub org-superstar org-sticky-header org-plus-contrib nov hl-todo helm-swoop helm-projectile helm-ag forge flycheck fira-code-mode expand-region ewal-doom-themes elisp-slime-nav edit-server doom-modeline diminish crux counsel-projectile common-lisp-snippets cdlatex ccls c-eldoc avy auctex all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired activity-watch-mode))
  '(smooth-scrolling-mode t)
  '(svg-tag-default-line-width 2)
  '(svg-tag-horizontal-offset 9)
