6d65971347211bdf29067dd9fc0c64667cd4936c
Kalelzar <natomanofglory@gmail.com>
2022-02-01 16:51:59 +0200

    Add support for controlling xbacklight from inside emacs

---
+++ modules/module-xbacklight.el
@@ -0,0 +1,62 @@
+;;; module-xbacklight.el --- A module for controlling the monitor backlight via xbacklight  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Borislav Atanasov
+
+;; Author: Borislav Atanasov <natomanofglory@gmail.com>
+;; Keywords: terminals, convenience, hardware, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defun xbacklight-get () (string-to-number (shell-command-to-string "xbacklight | tr -d '\n'")))
+(defun xbacklight-act (op value)
+  (shell-command-to-string (format "xbacklight '%s%d'" op value)))
+
+(defun xbacklight-inc (value)
+  (interactive (list (read-number "Increment by: ")))
+  (xbacklight-act "+" value))
+
+(defun xbacklight-dec (value)
+  (interactive (list (read-number "Decrement by: ")))
+  (xbacklight-act "-" value))
+
+(defun xbacklight-set (value)
+  (interactive (list (read-number "Set to: ")))
+  (xbacklight-act "=" value))
+
+(defun xbacklight-show ()
+  (interactive)
+  (exwm-show-msg (make-progress-bar (xbacklight-get) :width 80
+                                    :label (format "[Brightness: %d%%]" (xbacklight-get)))
+                 :center t))
+
+(defvar xbacklight-step 5)
+
+(defun xbacklight-inc-dwim ()
+  (interactive)
+  (xbacklight-inc xbacklight-step)
+  (xbacklight-show))
+
+(defun xbacklight-dec-dwim ()
+  (interactive)
+  (xbacklight-dec xbacklight-step)
+  (xbacklight-show))
+
+(provide 'module-xbacklight)
+;;; module-xbacklight.el ends here
