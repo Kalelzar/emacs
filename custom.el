84ccdab82af24bc6603834bc3befa9702f945bac
Kalelzar <natomanofglory@gmail.com>
2022-02-04 07:24:44 +0200

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   '(magit which-key vterm vertico use-package undo-tree super-save smooth-scrolling pulseaudio-control posframe pinentry org-contrib orderless marginalia kind-icon hydra exwm embark-consult emacsql-sqlite doom-themes doom-modeline diminish crux corfu consult-projectile bar-cursor all-the-icons-completion)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(exwm-alert-face ((t (:extend t :height 1.6 :family "Source Sans-Serif Pro")))))
+ )
